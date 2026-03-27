e81f81aafb72fd34e396b5ce783ea2cc16fc7cf9
Krishna <krishna9842740944@gmail.com>
2026-03-28 00:16:58 +0530

    Simple ToDoList

---
+++ script.js
@@ -0,0 +1,42 @@
+const toDolist=[{
+    name:'wash clothes',
+    dueDate:'2026-03-27'},
+    {
+        name:'watch youtube',
+dueDate:'2026-03-28'}];
+renderToDoList();
+
+function renderToDoList(){
+let  toDolistHTML='';
+for(let i=0;i<toDolist.length;i++){
+    const todoObject=toDolist[i];
+   // const name=todoObject.name;
+   // const dueDate=todoObject.dueDate;
+    const {name,dueDate}=todoObject;
+    const html =`
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+        toDolist.splice(${i},1);
+        renderToDoList();
+    "class="delete-button">Delete</button>
+    `;
+    toDolistHTML+=html;
+}
+ document.querySelector('.js-display-task').innerHTML=toDolistHTML;
+}
+
+function addTask(){
+    const inputElement= document.querySelector('.js-input');
+     const name=inputElement.value;
+
+     const dateInputElement=document.querySelector('.js-date-input');
+     const dueDate=dateInputElement.value;
+     toDolist.push({name:name,
+        dueDate:dueDate}
+     ); 
+     
+     inputElement.value='';
+     renderToDoList();
+}
+
