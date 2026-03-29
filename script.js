279cceb0e0b8f68573ba97785c07780be49f592b
Krishna <krishna9842740944@gmail.com>
2026-03-29 09:10:46 +0530

    Updated JS & CSS styling

--- script.js
+++ script.js
@@ -8,25 +8,32 @@ renderToDoList();
 
 function renderToDoList(){
 let  toDolistHTML='';
-toDolist.forEach((todoObject,index)=>{
+toDolist.forEach((todoObject)=>{
    // const name=todoObject.name;
    // const dueDate=todoObject.dueDate; 
     const {name,dueDate}=todoObject;
     const html =`
     <div>${name}</div>
     <div>${dueDate}</div>
-    <button onclick="
-        toDolist.splice(${index},1);
-        renderToDoList();
-        <!-- whenever we update the todo list, save in localStorage . -->
-        saveStorage();
-    "class="delete-button">Delete</button>
+    <button class="delete-button js-delete-button">Delete</button>
     `;
     toDolistHTML+=html;
 })
+document.querySelector('.js-display-task')
+.innerHTML=toDolistHTML;
 
- document.querySelector('.js-display-task').innerHTML=toDolistHTML;
+document.querySelectorAll('.js-delete-button').forEach((deleteButton,index)=>{
+    deleteButton.addEventListener('click',()=>{
+         toDolist.splice(index,1);
+        renderToDoList();
+        saveStorage();
+    })
+})
+
 }
+document.querySelector('.js-add-button').addEventListener('click',()=>{
+    addTask();
+});
 
 function addTask(){
     const inputElement= document.querySelector('.js-input');
